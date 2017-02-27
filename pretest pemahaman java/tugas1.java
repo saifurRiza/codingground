1f54cdcfa9ca413026f0960803ee80f426bd06e1
saifurRiza <saifur.riza22@gmail.com>
2017-02-27 12:35:12 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/tugas1.java
@@ -0,0 +1,8 @@
+public class tugas1{
+
+     public static void main(String []args){
+         String nama = " saifur";
+         String kota = " jepara";
+        System.out.println("nama : " + nama + " kota: " + kota);
+     }
+}
