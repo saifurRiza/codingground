32ead3e87495243cf71dfd5e5c23c22b83305184
saifurRiza <saifur.riza22@gmail.com>
2017-02-27 12:35:51 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/tugas2.java
@@ -0,0 +1,9 @@
+public class tugas2{
+    
+         public static void main(String []args){
+        String nama = "saifur";
+        nama = nama + " " + "riza";
+        System.out.println(nama);
+
+}
+}
